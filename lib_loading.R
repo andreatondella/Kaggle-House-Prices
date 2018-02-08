09c0f4af21f6a667189626f19e41a6d865faf1a0
Andrea Tondella <andrea@Andreas-MacBook-Pro.local>
2018-02-08 16:01:41 +0100

    Initial Load

---
+++ lib_loading.R
@@ -0,0 +1,17 @@
+# ANDREA TONDELLA
+# IE MBD - Class O2
+# Oct. 2017 Intake
+
+# Machine Learning 2 - Individual Assignment
+# Kaggle House Prices
+
+### ---------- Library loading and java setting ----------
+
+dyn.load('/Library/Java/JavaVirtualMachines/jdk-9.0.4.jdk/Contents/Home/lib/server/libjvm.dylib')
+library(ggplot2)
+library(plyr)
+library(dplyr)
+library(moments)
+library(glmnet)
+library(caret)
+library(FSelector)
