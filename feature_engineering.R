9c60d42dfec9270e7048987c30dc308614ba129e
Andrea Tondella <andrea@Andreas-MacBook-Pro.local>
2018-02-09 13:30:56 +0100

    First push

---
+++ feature_engineering.R
@@ -0,0 +1,9 @@
+# ANDREA TONDELLA
+# IE MBD - Class O2
+# Oct. 2017 Intake
+
+# Machine Learning 2 - Individual Assignment
+# Kaggle House Prices
+
+source("lib_loading.R")
+source("data_cleaning.R")
