97c717648bfd65441aad6b6137642f613b01e5be
Andrea Tondella <andrea@Andreas-MacBook-Pro.local>
2018-02-18 22:08:12 +0100

    played around with the parameter

--- lasso_model.R
+++ lasso_model.R
@@ -17,22 +17,31 @@ source("information_gain.R")
 # Evaluation
 lambdas <- 10^seq(-3, 3, by = .1)
 
-lasso.cv_fit <- cv.glmnet(x = data.matrix(training[, !"SalePrice", with = F]), y = training$SalePrice, alpha = 0.75, lambda = lambdas, nfolds = 20)
+lasso.cv_fit <- cv.glmnet(x = data.matrix(training[, !"SalePrice", with = F]), y = training$SalePrice, alpha = 1.5, lambda = lambdas, nfolds = 20)
 plot(lasso.cv_fit)
 
 # Select the best lambda form the CV model, use it to predict the target value of the validation set and evaluate the results (in terms of RMSE)
 bestlam <- lasso.cv_fit$lambda.min
 paste("Best Lambda value from CV=", bestlam)
-lasso.mod <- glmnet(x = data.matrix(training[, !"SalePrice", with = F]), y = training$SalePrice, alpha = 0.75, lambda = lambdas)
+lasso.mod <- glmnet(x = data.matrix(training[, !"SalePrice", with = F]), y = training$SalePrice, alpha = 1.5, lambda = lambdas)
 lasso.pred = predict(lasso.mod, s=bestlam, data.matrix(validation[, !"SalePrice", with = F]))
 paste("RMSE for lambda ", bestlam, " = ", sqrt(mean((lasso.pred - validation$SalePrice)^2)))
 
 # Select the λ1se value from the CV model to predict on the validation set
 lam1se <- lasso.cv_fit$lambda.1se
 paste("Lambda 1se value from CV=", lam1se)
-lasso.mod <- glmnet(x = data.matrix(training[, !"SalePrice", with = F]), y=training$SalePrice, alpha = 0.75, lambda = lambdas)
+lasso.mod <- glmnet(x = data.matrix(training[, !"SalePrice", with = F]), y=training$SalePrice, alpha = 1.5, lambda = lambdas)
 lasso.pred=predict(lasso.mod, s=lam1se, data.matrix(validation[, !"SalePrice", with = F]))
 paste("RMSE for lambda ", lam1se, " = ", sqrt(mean((lasso.pred - validation$SalePrice)^2)))
+
+# Plot important coefficients 
+my_data=as.data.frame(cbind(predicted=lasso.pred,observed=validation$SalePrice))
+
+ggplot(my_data,aes(my_data["1"],observed))+
+  geom_point()+geom_smooth(method="lm")+
+  scale_x_continuous(expand = c(0,0)) +
+  labs(x="Predicted") +
+  ggtitle('Ridge')
 
 # Prediction on the test data
 test_data <- data.table(test_data)
@@ -47,4 +56,4 @@ submit$SalePrice[is.na(submit$SalePrice)] <- 0
 replace_value_for_na <- sum(na.omit(submit$SalePrice))/(nrow(submit) - sum(submit$SalePrice == 0))
 submit$SalePrice[submit$SalePrice == 0] <- replace_value_for_na
 
-write.csv(submit,file="submission_V11.csv",row.names=F)
+write.csv(submit,file="submission_V28.csv",row.names=F)
